39f0040375235d2288f67d72f217b6a3b7833052
waqaspassu <waqasquba@gmail.com>
2024-01-23 13:53:29 +0500

    added cart to ecom hippo app

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,90 @@
-const Cart = ()=> {
-    return (
-        <div>
-            This is cart Component
-        </div>
-    )
-}
+"use client";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button, buttonVariants } from "./ui/button";
+import { ShoppingCartIcon } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
+
+const Cart = () => {
+  const itemCount = 0;
+  const fee =2;
+  return (
+    <Sheet>
+      <SheetTrigger className="group -m-2 flex items-center p-2">
+        <ShoppingCartIcon className="h-6  w-6 flex-shrink-0 tex-gray-400 group-hover:text-gray-500" />
+        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+          0
+        </span>
+      </SheetTrigger>
+      <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+        <SheetHeader className="space-y-2.5 pr-6">
+          <SheetTitle className="text-sm font-medium text-gray-900">
+            {itemCount > 0 ? (
+              <>
+                <div className="flex w-full flex-col pr-6">
+                  {/* Cart Logic */}
+                  cart items
+                </div>
+                <div className="space-y-4 pr-6">
+                  <Separator />
+                  <div className="space-y-1.5 pr-6 ">
+                    <div className="flex">
+                      <span className="flex-1">Shipping</span>
+                      <span>Free</span>
+                    </div>
+                    <div className="flex">
+                      <span className="flex-1">Trasaction Fee</span>
+                      <span>{formatPrice(fee)}</span>
+                    </div><div className="flex">
+                      <span className="flex-1">Total</span>
+                      <span>{formatPrice(fee)}</span>
+                    </div>
+                  </div>
+                  <SheetFooter>
+                    <SheetTrigger asChild>
+                    <Link href="/cart" className={buttonVariants({className:"w-full"})}>Contine to Checkout</Link>
+                    </SheetTrigger>
+                  </SheetFooter>
+                </div>
+              </>
+            ) : (
+              <div className="flex h-full flex-col items-center justify-center space-y-2">
+                <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+                    <Image src="/hippo-empty-cart.png" alt="Empty cart" fill  />
+                </div>
+                <div className="text-xl font-semibold">
+                <SheetTrigger asChild>
+                    <Link href="/products" className={buttonVariants({
+                        variant:"link",
+                        size:"sm",
+                        className:"text-sm text-muted-foreground"
+                    })}>
+                        Add item to your cart to checkout
+                    </Link>
+                </SheetTrigger>
+                </div>
+              </div>
+            )}
+          </SheetTitle>
+          <SheetDescription className="text-sm text-gray-500">
+            {itemCount < 0 && "Your shopping cart is empty"}
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 export default Cart;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(price: number | string, options:{
+  currency?: "USD" | "EUR" | "GBP" | "BDT",
+  notation?: Intl.NumberFormatOptions["notation"],
+} ={}
+){
+const {currency = "USD" ,notation = "compact"} = options;
+const numericPrice = typeof price !== "number" ? parseFloat(price): price;
+return new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency,
+  notation,
+  maximumFractionDigits:2
+}).format(numericPrice);
+}
