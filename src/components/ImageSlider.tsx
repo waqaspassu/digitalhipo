011a5ac1c21a6a1f58e8e5ad9193477f4e6f6f65
waqaspassu <waqasquba@gmail.com>
2024-02-24 13:48:43 +0500

    added airBnd type slider

---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import SwiperType from "swiper";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import { ChevronDown, ChevronLeft, ChevronRight } from "lucide-react";
+
+interface ImageSliderProps {
+  urls: string[];
+}
+const ImageSlider = ({ urls }: ImageSliderProps) => {
+  const [swiper, setSwiper] = useState<null | SwiperType>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [slidesConfig, setSlidesConfig] = useState({
+    isBegning: true,
+    isEnd: activeIndex === (urls.length ?? 0) - 1,
+  });
+
+  useEffect(() => {
+    console.log({ urls });
+    swiper?.on("slideChange", ({ activeIndex }) => {
+      setActiveIndex(activeIndex);
+      setSlidesConfig({
+        isBegning: activeIndex === 0,
+        isEnd: activeIndex === (urls.length ?? 0) - 1,
+      });
+    });
+  }, [swiper, urls]);
+  const activeStyle =
+    "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
+  const inActiceStyles = "hidden text-gray-400";
+  return (
+    <div className="group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
+      <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slideNext();
+          }}
+          className={cn(activeStyle, "right-4  transition", {
+            [inActiceStyles]: slidesConfig.isEnd,
+            "hover:bg-primary-300 text-primary-800 opacity-100":
+              !slidesConfig.isEnd,
+          })}
+          aria-label="next-image"
+        >
+          <ChevronRight className="h-4 w-4 text-zinc-700" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slidePrev();
+          }}
+          className={cn(activeStyle, "left-4  transition", {
+            [inActiceStyles]: slidesConfig.isBegning,
+            "hover:bg-primary-300 text-primary-800 opacity-100":
+              !slidesConfig.isBegning,
+          })}
+          aria-label="previous-image"
+        >
+          <ChevronLeft className="h-4 w-4 text-zinc-700" />
+        </button>
+      </div>
+      <Swiper
+        pagination={{
+          renderBullet: ({_, className}:any) => {
+            return `<span class="rounded-full transition ${className}"></span>`
+          },
+        }}
+        onSwiper={(swiper) => setSwiper(swiper)}
+        spaceBetween={50}
+        slidesPerView={1}
+        modules={[Pagination]}
+        className="h-full w-full"
+      >
+        {urls.map((url, index) => {
+          return (
+            <SwiperSlide key={index} className="-z-10 relative w-full h-full">
+              <Image
+                src={url}
+                loading="eager"
+                alt="product image category"
+                fill
+                className="-z-10 h-full object-cover object-center"
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ImageSlider;
--- src/components/ProductsListings.tsx
+++ src/components/ProductsListings.tsx
@@ -1,51 +1,68 @@
-import { Product } from "@/payload-types"
+import { Product } from "@/payload-types";
 import { useEffect, useState } from "react";
 import { Skeleton } from "./ui/skeleton";
 import Link from "next/link";
 import { cn, formatPrice } from "@/lib/utils";
 import { PRODUCT_CATEGORY } from "@/config";
+import ImageSlider from "./ImageSlider";
 
 interface ProductListingProps {
-    product: Product | null;
-    index:number
+  product: Product | null;
+  index: number;
 }
 
-const ProductListing = ({product, index}:ProductListingProps) =>{
+const ProductListing = ({ product, index }: ProductListingProps) => {
+  const [visile, setIsvisible] = useState<boolean>(false);
 
-    const [visile , setIsvisible] = useState<boolean>(false)
+  const label = PRODUCT_CATEGORY.find(
+    ({ value }) => value === product?.category
+  )?.label;
 
-    const label = PRODUCT_CATEGORY.find(({value})=> value === product?.category )?.label
-useEffect(()=>{
-    const timeer = setTimeout(()=>{
-        setIsvisible(true)
-    }, index*75)
+  const validUrls = product?.images
+    .map(({ image }) => (typeof image === "string" ? image : image?.url))
+    .filter(Boolean) as string[];
+  useEffect(() => {
+    const timeer = setTimeout(() => {
+      setIsvisible(true);
+    }, index * 75);
 
-    return ()=> clearTimeout(timeer)
-},[index])
+    return () => clearTimeout(timeer);
+  }, [index]);
 
-    if(product && !visile) return <ProductPlaceholder />
-    if(visile && product) return <Link className={cn(`invisible h-full w-full cursor-pointer group/main`,{
-        "visible animate-in fade-in-5":visile
-    })} href={`/product/${product.id}`}>
+  if (product && !visile) return <ProductPlaceholder />;
+  if (visile && product)
+    return (
+      <Link
+        className={cn(`invisible h-full w-full cursor-pointer group/main`, {
+          "visible animate-in fade-in-5": visile,
+        })}
+        href={`/product/${product.id}`}
+      >
         <div className="flex flex-col w-full">
-            <h3 className="mt-4 text-sm font-medium text-grey-700">{product.name}</h3>
-            <p className="mt-1 text-sm text-green-500">{label}</p>
-            <p className="mt-1 font-medium text-grey-900">{formatPrice(product.price)}</p>
+          <ImageSlider urls={validUrls} />
+          <h3 className="mt-4 text-sm font-medium text-grey-700">
+            {product.name}
+          </h3>
+          <p className="mt-1 text-sm text-green-500">{label}</p>
+          <p className="mt-1 font-medium text-grey-900">
+            {formatPrice(product.price)}
+          </p>
         </div>
-    </Link>
-}
+      </Link>
+    );
+};
 
-const ProductPlaceholder = () =>{
-    return(
-        <div className="flex flex-col w-full">
-            <div className="relative w-full bg-zinc-100 aspect-square overflow-hidden rounded-xl">
-                <Skeleton className="h-full w-full"/>
-            </div>
-            <Skeleton className="mt-4 w-2/3 h-4 rounded-lg" />
-            <Skeleton className="mt-2 w-16 h-4 rounded-lg" />
-            <Skeleton className="mt-4 w-12 h-4 rounded-lg" />
-        </div>
-    )
-}
+const ProductPlaceholder = () => {
+  return (
+    <div className="flex flex-col w-full">
+      <div className="relative w-full bg-zinc-100 aspect-square overflow-hidden rounded-xl">
+        <Skeleton className="h-full w-full" />
+      </div>
+      <Skeleton className="mt-4 w-2/3 h-4 rounded-lg" />
+      <Skeleton className="mt-2 w-16 h-4 rounded-lg" />
+      <Skeleton className="mt-4 w-12 h-4 rounded-lg" />
+    </div>
+  );
+};
 
-export default ProductListing
+export default ProductListing;
