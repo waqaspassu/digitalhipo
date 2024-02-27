84bdb72aa1e397af62a1c1fae4d9cf04cbd08c3a
waqaspassu <waqasquba@gmail.com>
2024-02-27 10:59:17 +0500

    added protected route to app

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,5 +1,22 @@
-import { router } from "./trpc";
+import { z } from "zod";
+import { privateProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+import { getPayloadClient } from "../get-payload";
 
 export const PaymentRouter = router({
-    createSession: privateProcedure
-})
+  createSession: privateProcedure
+    .input(z.object({ productIds: z.array(z.string()) }))
+    .mutation(async ({ ctx, input }) => {
+      const { user } = ctx;
+      const { productIds } = input;
+      const payload = await getPayloadClient();
+      const {docs:product} = await payload.find({
+        collection: "products",
+        where: {
+          id: {
+            in: productIds,
+          },
+        },
+      });
+    }),
+});
